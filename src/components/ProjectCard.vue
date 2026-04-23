6cd392464be72308e4cc2244a547bd9f61489d7c
peralta.dan1208 <peralta.dan1208@gmail.com>
2026-04-23 13:28:28 +0800

    UI Improvements

--- src/components/ProjectCard.vue
+++ src/components/ProjectCard.vue
@@ -1,12 +1,11 @@
 <template>
-	<div class="card col-md-3">
-		<img :src="project.image" class="card-img-top card-img" alt="...">
+	<div class="card col-md-3 m-2 rounded shadow" id="carouselItem">
+		<img :src="project.image" class="card-img-top card-img mt-3" alt="...">
 		<div class="card-body">
 			<h5 class="card-title">{{project.title}}</h5>
 			<p class="card-text">
 				{{project.description}}
 			</p>
-
 		</div>
 	</div>
 </template>
--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,46 +1,33 @@
 <template>
-  <div id="projects" class="container mb-3">
+  <div id="projects" class="container mb-5">
     <h1 class="text-center pt-5 mt-5">My Projects</h1>
-
     <div class="row p-5 justify-content-center">
       <div id="carouselExampleControls" class="carousel slide" data-ride="carousel" data-interval="false">
         <div class="carousel-inner">
-          <div class="carousel-item" v-for="(group, index) in chunkedProjects" :key="index" :class="{ active: index === 0 }" >
+          <div class="carousel-item" v-for="(group,index) in chunkedProjects" :key="index" :class="{ active: index === 0 }">
             <div class="row justify-content-center">
-              <div class="col-md-3 m-2 rounded shadow" id="carouselItem" v-for="project in group" :key="project.id" >
-                <img :src="project.image" class="img-fluid mx-auto d-block p-3" />
-                <h3>{{ project.title }}</h3>
-                <p>{{ project.description }}</p>
-              </div>
+              <ProjectCard v-for="project in group" :key="project.id" :project="project"/>
             </div>
           </div>
-
         </div>
-
-        <!-- Controls -->
         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev" >
-          <span class="carousel-control-prev-icon"></span>
-        </a>
-
-        <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" >
-          <span class="carousel-control-next-icon"></span>
-        </a>
-
+            <span class="carousel-control-prev-icon"></span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next" >
+            <span class="carousel-control-next-icon"></span>
+          </a>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-	// import the 'computed' method to define reactive derived state
 	import { computed } from 'vue';
 	import ProjectCard from './ProjectCard.vue';
 	import projects from '../data/projects.json';
 
-	// number of projects displayed per row
 	const chunkSize = 3;
 
-	// the main purpose of the function is to create smaller arrays inside of the projects that contains 3(chunkSize) projects each
 	const chunkedProjects = computed(() => {
 		const chunks = [];
 
